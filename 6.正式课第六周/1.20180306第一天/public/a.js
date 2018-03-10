08e6ad1728d224d96136f69b9a55cbe4e8ca6c49
zhufengpeixun <1144709265@qq.com>
2018-03-10 13:03:47 +0800

    1

---
+++ 6.正式课第六周/1.20180306第一天/public/a.js
@@ -0,0 +1,5 @@
+let  fn =(item,index)=>{
+    let a = 10;
+
+    console.log(12)
+}
---
+++ 6.正式课第六周/1.20180306第一天/public/b.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var fn = function fn(item, index) {
+    var a = 10;
+
+    console.log(12);
+};
