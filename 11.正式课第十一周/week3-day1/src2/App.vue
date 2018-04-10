a4afed3f1b60267da3ba53b7e54818f43d27425c
zhufengpeixun <1144709265@qq.com>
2018-04-10 18:22:18 +0800

    1223

---
+++ 11.正式课第十一周/week3-day1/src2/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="header">
+          123
+        <button @click="fn">{{msg}}</button>
+    </div>
+</template>
+<script>
+
+    export default {
+        data(){
+            // 对象数据类型，return + {}
+            return {msg:"你很帅"}
+        },
+        methods:{
+            fn(){
+                alert(1)
+            }
+        },
+        components:{
+
+        }
+
+    }
+</script>
+<style  scoped  lang="less">
+    /*让当前样式变成私有scoped*/
+    /*lang="less",支持less语法；*/
+    /*全局样式*/
+    .header{
+
+            width:300px;
+            line-height: 100px;
+            height:100px;
+            background: lightblue;
+            margin:auto;
+
+    }
+    /**{*/
+        /*margin:0;*/
+        /*padding:0;*/
+    /*}*/
+    /*a{text-decoration: none}*/
+    /*ul{list-style: none}*/
+
+</style>
